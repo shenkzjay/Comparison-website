7dccb84fa81f9cba2838bc83ac90ac2be27cf825
shenkzjay <senksjay@gmail.com>
2023-11-07 17:59:39 +0100

    fixed issue with test config

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,6 @@ const config = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 
   testEnvironment: "jest-environment-jsdom",
-
-  preset: "ts-jest",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
